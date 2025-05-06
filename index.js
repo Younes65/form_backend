828ee46ff4a453f1a56bd12831301828964d8139
Ibrahim Younes <ibrahimyounes@Mac.lan>
2025-05-06 21:55:50 +0400

    edits

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const upload = multer({ storage: multer.memoryStorage() });
 
 const corsOption = {
-  origin: ["https://application-form-gamma-seven.vercel.app"],
+  origin: "https://application-form-gamma-seven.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
